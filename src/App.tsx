c6a92b81c849cfde06e7f98e464889094a7a9e46
MayurThorat319 <mayurthorat319@gmail.com>
2025-08-09 11:57:24 +0530

    description font changes

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const carouselItems = [
     id: 1,
     title: 'Jogging Track',
     description:
-      'A scenic route for daily jogs, evening walks, and weekend runs where staying fit feels like an escape into nature.',
+      'TAKE A WALK, GO FOR A JOG, OR ENJOY A QUIET RUN ALONG THE JOGGING TRACK. IT’S A GREAT WAY TO MOVE YOUR BODY WHILE SOAKING IN THE VIEWS. FITNESS FEELS EASY WHEN IT LOOKS THIS GOOD.',
     image: '/images/jogging_track.png',
     backgroundImage: '/background_images/blur_jogging_track.png',
     caption: 'Jogging Track',
@@ -58,7 +58,7 @@ const carouselItems = [
     id: 2,
     title: 'Exclusive Gym',
     description:
-      'A welcoming, well-stocked gym with great equipment and scenic views ,making it easy to stay fit and feel good every time you step inside.',
+      'STEP INTO OUR WELCOMING, WELL-EQUIPPED GYM WHERE EVERY CORNER IS DESIGNED TO INSPIRE YOUR FITNESS JOURNEY. LATEST MACHINES AND STUNNING SCENIC VIEWS, WORKING OUT BECOMES A REFRESHING EXPERIENCE THAT LIFTS BOTH BODY AND SPIRIT.',
     image: '/images/GYM.png',
     backgroundImage: '/background_images/blur_gym.png',
     caption: 'Exclusive Gym',
@@ -68,7 +68,7 @@ const carouselItems = [
     id: 3,
     title: 'Infinity Pool',
     description:
-      'An inviting infinity pool that feels like it touches the sky , great for a cool swim with lovely views all around.',
+      'THE INFINITY POOL OFFERS A SERENE ESCAPE WHERE THE WATER APPEARS TO MERGE WITH THE SKY. ENJOY A COOL, REFRESHING SWIM SURROUNDED BY BREATHTAKING VIEWS. IT’S THE PERFECT PLACE TO RELAX, RECHARGE, AND TAKE IN THE BEAUTY AROUND YOU.',
     image: '/images/Infinity_pool.png',
     backgroundImage: '/background_images/blur_infinity_pool.png',
     caption: 'Infinity Pool',
@@ -78,7 +78,7 @@ const carouselItems = [
     id: 4,
     title: 'Meditation Centre',
     description:
-      'A serene haven to find calm and reconnect with yourself where stunning views and mindful moments renew your body, mind and soul.',
+      'A SERENE SANCTUARY TO HELP YOU RECONNECT WITH YOUR INNER SELF AND FIND PEACE AMID LIFE’S HUSTLE. NESTLED IN A TRANQUIL SETTING WITH STUNNING VIEWS, OUR CENTER OFFERS A PERFECT BLEND OF MINDFULNESS, SPIRITUALITY, AND RELAXATION TO REFRESH YOUR MIND, BODY, AND SOUL',
     image: '/images/Meditation_center.png',
     backgroundImage: '/background_images/blur_meditation_center.png',
     caption: 'Meditation Centre',
@@ -91,7 +91,7 @@ const communityCornersItems = [
     id: 1,
     title: 'Rooftop Lounge',
     description:
-      "Whether It's A Sunset Cocktail, A Romantic Dinner, Or A Vibrant Celebration, Our Rooftop Party Lounge With Stunning Sea Views Promises A Magical Experience like No Other, Where Every Moment Becomes A Cherished Memory.",
+      "WHETHER IT’S A SUNSET COCKTAIL, A ROMANTIC DINNER, OR A LIVELY CELEBRATION, OUR ROOFTOP PARTY LOUNGE WITH STUNNING SEA VIEWS OFFERS A MAGICAL EXPERIENCE. EVERY MOMENT HERE BECOMES A CHERISHED MEMORY AGAINST THE BACKDROP OF BREATHTAKING SCENERY. JOIN US AND MAKE YOUR SPECIAL OCCASIONS TRULY UNFORGETTABLE.",
     image: '/images/rooftop_lounge.png',
     backgroundImage: '/background_images/blur_rooftop_lounge.png',
     caption: 'Rooftop Lounge',
@@ -101,7 +101,7 @@ const communityCornersItems = [
     id: 2,
     title: 'Barbeque Zone',
     description:
-      'Gather, grill, and enjoy! Our Barbeque Zone is perfect for family and friends to savor tasty grilled meals and create lasting memories. With great grilling stations and a cozy vibe, let’s fire up the flavors for every gathering!',
+      'FIRE UP THE GRILL AND BRING EVERYONE TOGETHER IN OUR LIVELY BARBECUE ZONE. IT’S THE IDEAL PLACE TO ENJOY GREAT FOOD, GOOD COMPANY, AND PLENTY OF SMILES. MAKE EVERY MEAL A CELEBRATION AND EVERY MOMENT A MEMORY.',
     image: '/images/Barbeque_zone.png',
     backgroundImage: '/background_images/blur_barbeque_zone.png',
     caption: 'Barbeque Zone',
@@ -111,7 +111,7 @@ const communityCornersItems = [
     id: 3,
     title: 'Marina Bay Garden',
     description:
-      'Discover tranquility in our lush garden, a vibrant oasis of colorful flowers, towering trees, and beautifully landscaped paths.',
+      'STEP INTO A VIBRANT GARDEN BURSTING WITH COLORFUL BLOOMS, TREES, AND SECRET PATHS WAITING TO BE DISCOVERED. EACH CORNER OFFERS A NEW CHANCE TO PAUSE, BREATHE, AND LET NATURE’S BEAUTY WASH OVER YOU. IT’S YOUR EVERYDAY ESCAPE TO PEACE.',
     image: '/images/Marina_bay_garden.png',
     backgroundImage: '/background_images/blur_marina_bay_garden.png',
     caption: 'Marina Bay Garden',
@@ -121,7 +121,7 @@ const communityCornersItems = [
     id: 4,
     title: 'Kids Play Area',
     description:
-      'A vibrant space designed to inspire joy and creativity for kids of all ages. Our play area blends fun, safety, and imagination, creating a haven for exploration and learning.',
+      'A VIBRANT AND EXCITING SPACE BUILT TO SPARK JOY, CREATIVITY, AND ADVENTURE FOR CHILDREN OF ALL AGES. OUR PLAY AREA IS CAREFULLY DESIGNED WITH FUN, SAFETY, AND IMAGINATION IN MIND. IT’S A WELCOMING PLACE WHERE KIDS CAN EXPLORE, LEARN, AND GROW.',
     image: '/images/Kids_play_area.png',
     backgroundImage: '/background_images/blur_kids_play_area.png',
     caption: 'Kids Play Area',
